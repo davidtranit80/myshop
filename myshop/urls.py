4307f1da274a8cab74e5cffe75860b53e23828d7
David Tran <davidtranit80@gmail.com>
2020-04-23 16:37:58 +0930

    Change title of admin site

--- myshop/urls.py
+++ myshop/urls.py
@@ -19,3 +19,7 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+admin.site.site_header = "MyShop Admin"
+admin.site.site_title = "MyShop Admin Portal"
+admin.site.index_title = "Welcome to MyShop Admin Portal"
