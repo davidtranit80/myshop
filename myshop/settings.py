724a34fd5ae4fde53ae578139a116345cde8351e
David Tran <davidtranit80@gmail.com>
2020-04-23 22:52:54 +0930

    add aws eb server to allowed host, changes version required psycopg2 in requirements.txt, add .gitignore file

--- myshop/settings.py
+++ myshop/settings.py
@@ -80,7 +80,29 @@ WSGI_APPLICATION = 'myshop.wsgi.application'
 #     }
 # }
 
-DATABASES = {
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'myshop',
+#         'USER': 'nhon',
+#         'PASSWORD': 'huongu83',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+else:
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'myshop',
@@ -90,7 +112,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
