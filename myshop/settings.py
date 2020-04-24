78b388383869dbdab6714d3cce1e2e712d1328ec
David Tran <davidtranit80@gmail.com>
2020-04-24 20:59:13 +0930

    add .rds.amazonaws.com to allowed_hosts

--- myshop/settings.py
+++ myshop/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = [
     'localhost',
     '.compute-1.amazonaws.com', # allows viewing of instances directly
     '.elasticbeanstalk.com',
-    '*'
+    '.rds.amazonaws.com'
 ]
 
 # Application definition
